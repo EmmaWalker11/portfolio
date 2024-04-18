10b78f6b2f24a89e2bbd53ef8e9426fdbb5e2fec
Emma Walker <emma.walker11@uon.edu.au>
2024-04-18 22:52:36 +1000

    Nav bar moves when screen moves to hide behind the hamburger. need to do the hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,65 +1,85 @@
-import React, {useState} from 'react'
-// import './NavBar.css';
+// import React, {useState} from 'react'
+import React from 'react';
+import Logo from '../images/logo192.png';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import './NavBar.css';
 // import { Link } from 'react-router-dom';
 
-function NavBar() {
+const NavBar = () => {
+    return(
+        <div className="navBar">
+            <div className='logo'>
 
-    const [click, setClick] = useState(false);
-
-    const handleClick = () => setClick(!click);
-
-    const closeMobileNav = () => setClick(false);
-
-
-    return (
-        <>
-        {/* <header>
-            <div>
-                <a href
+                <img src={Logo} alt='logo' />
+                
             </div>
-        </header> */}
-         <nav className='navBar'>
-            <div className='navMenu'>
-                {/* <Link to="/" >Emma</Link> */}
-                <div className='navMenuIcon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fa-solid fa-bars'} />
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='navItem'> <a href='#home' className='navlink' onClick={closeMobileNav} >Home</a></li>
-                        <li className='navItem'> <a href='#about' className='navlink' onClick={closeMobileNav} >About</a></li>
-                        <li className='navItem'> <a href='#projects' className='navlink' onClick={closeMobileNav} >Projects</a></li>
-                        <li className='navItem'> <a href='#contact' className='navlink' onClick={closeMobileNav} >Contact</a></li>
-                    </ul>
-                </div>
+            <ul className='navMenu'>
+                <li className='navItem'><a href='#home'>Home</a></li>
+                <li className='navItem'><a href='#about'>About</a></li>
+                <li className='navItem'><a href='#projects'>Projects</a></li>
+                <li className='navItem'><a href='#contact'>Contact</a></li>
+            </ul>
+            <div className='hamburger'>
+                <FaBars size={30}/>
             </div>
+        </div>
+
+    // const [click, setClick] = useState(false);
+
+    // const handleClick = () => setClick(!click);
+
+    // const closeMobileNav = () => setClick(false);
+
+
+    // return (
+    //     <>
+    //     {/* <header>
+    //         <div>
+    //             <a href
+    //         </div>
+    //     </header> */}
+    //      <nav className='navBar'>
+    //         <div className='navMenu'>
+    //             {/* <Link to="/" >Emma</Link> */}
+    //             <div className='navMenuIcon' onClick={handleClick}>
+    //                 <i className={click ? 'fas fa-times' : 'fa-solid fa-bars'} />
+    //                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+    //                     <li className='navItem'> <a href='#home' className='navlink' onClick={closeMobileNav} >Home</a></li>
+    //                     <li className='navItem'> <a href='#about' className='navlink' onClick={closeMobileNav} >About</a></li>
+    //                     <li className='navItem'> <a href='#projects' className='navlink' onClick={closeMobileNav} >Projects</a></li>
+    //                     <li className='navItem'> <a href='#contact' className='navlink' onClick={closeMobileNav} >Contact</a></li>
+    //                 </ul>
+    //             </div>
+    //         </div>
             
-        </nav>
+    //     </nav>
 
-        {/* <nav className='navbar'>
-            <div className='navMenu'>
-                <Link to="/" >Emma</Link>
-                <div className='navMenuIcon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fa-solid fa-bars'} />
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Home</Link></li>
-                        <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >About</Link></li>
-                        <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Projects</Link></li>
-                        <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Contact</Link></li>
-                    </ul>
-                </div>
-            </div>
+    //     {/* <nav className='navbar'>
+    //         <div className='navMenu'>
+    //             <Link to="/" >Emma</Link>
+    //             <div className='navMenuIcon' onClick={handleClick}>
+    //                 <i className={click ? 'fas fa-times' : 'fa-solid fa-bars'} />
+    //                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+    //                     <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Home</Link></li>
+    //                     <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >About</Link></li>
+    //                     <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Projects</Link></li>
+    //                     <li className='navItem'> <Link to="/" className='navlink' onClick={closeMobileNav} >Contact</Link></li>
+    //                 </ul>
+    //             </div>
+    //         </div>
             
-        </nav> */}
+    //     </nav> */}
 
-        </>
-        // <div className='NavBar'>
-        //     <ul className='NavMenu'>
-        //         <li> <a href='#about'>Emma Walker</a></li>
-        //         <li> <a href='#skills'>Skills</a></li>
-        //         <li> <a href='#contact'>Contact</a></li>
-        //         <li> <a href='#projects'>Projects</a></li>
+    //     </>
+    //     // <div className='NavBar'>
+    //     //     <ul className='NavMenu'>
+    //     //         <li> <a href='#about'>Emma Walker</a></li>
+    //     //         <li> <a href='#skills'>Skills</a></li>
+    //     //         <li> <a href='#contact'>Contact</a></li>
+    //     //         <li> <a href='#projects'>Projects</a></li>
     
-        //     </ul>
-        // </div>
+    //     //     </ul>
+    //     // </div>
         
     )
 }
