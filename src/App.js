1b632db945db00d69f4baf234ca66097b0711414
Emma Walker <emma.walker11@uon.edu.au>
2024-04-18 13:47:26 +1000

    Adding components to the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-// Use rfce to create new file templates
+//Note to self: Use rfce to create new file templates
+
 import React from 'react'
+import About from './components/About'
+import Contact from './components/Contact'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
   return (
@@ -7,6 +13,11 @@ function App() {
         <main>
             <h1>Tesing this works</h1>
             <p>another test</p>
+            <NavBar />
+            <About />
+            <Projects />
+            <Skills />
+            <Contact />
         
         </main>
     </div>
