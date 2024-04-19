c423c7effd41984aa57d07907a32342598e4dff8
Emma Walker <emma.walker11@uon.edu.au>
2024-04-19 11:08:01 +1000

    Nav bar now works with linking to parts of the webpage as regular and with hamburger menu (need to put in resume pdf)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,35 @@
-// import React, {useState} from 'react'
-import React from 'react';
+import React, {useState} from 'react'
+// import React from 'react';
 import Logo from '../images/logo192.png';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import './NavBar.css';
 // import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const handleCloseNavbar = () => setClick(false);
+
     return(
         <div className="navBar">
             <div className='logo'>
 
                 <img src={Logo} alt='logo' />
+                <h1>Emma Walker</h1>
                 
             </div>
-            <ul className='navMenu'>
-                <li className='navItem'><a href='#home'>Home</a></li>
-                <li className='navItem'><a href='#about'>About</a></li>
-                <li className='navItem'><a href='#projects'>Projects</a></li>
-                <li className='navItem'><a href='#contact'>Contact</a></li>
+            <ul className={click ? 'navMenu active' : 'navMenu'}>
+                {/* <li className='navItem'><a href='#home'>Home</a></li> */}
+                <li className='navItem'><a href='#about' onClick={handleCloseNavbar}>About</a></li>
+                <li className='navItem'><a href='#projects'onClick={handleCloseNavbar}>Projects</a></li>
+                <li className='navItem'><a href='#contact'onClick={handleCloseNavbar}>Contact</a></li>
+                <button className='resumeButton'onClick={handleCloseNavbar}>Resume</button>
             </ul>
-            <div className='hamburger'>
-                <FaBars size={30}/>
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes size={30}/>) : <FaBars size={30}/>}
             </div>
         </div>
         
