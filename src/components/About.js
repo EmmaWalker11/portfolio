280c98508ee3ea9b0d26c034a8910e86ba64276d
Emma Walker <emma.walker11@uon.edu.au>
2024-04-19 23:19:22 +1000

    Completing the about section. Just need a logo, screenshot of the website, say more projects are coming, add in resume, and connect form

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,8 @@ import React from 'react'
 import Skills from './Skills'
 import './About.css';
 import { experience } from "./Data.js";
+import Leaf from '../images/leaf.png';
+import IconLinks from './IconLinks';
 
 function About() {
   return (
@@ -22,12 +24,28 @@ function About() {
             </div>
             <div className='aboutWritingContainer'>
               <div className='getToKnowMeSection'>
-                <h3>Get to know me!</h3>
+                <h3 className='sectionHeading'>Get to know me!</h3>
                 <p>Hi! I'm Emma (she/her), a software engineering student with a passion to learn and adapt to new technologies. I am a current third year university student at the University of Newcastle studying a Bachelors of Software Engineering (honours). I have currently undergone 16 courses and have a gpa of 6.3/7. I am Newcastle based and seeking casual employment and summer internships. </p>
                 <p>Some cool things about me. I am deaf in one ear. I am a dual citizen of both America and Australia. Outside of being a software engineer, I am an artist with both traditional and digital art. I’m a photographer. And I play a bit of guitar. I also love video games, so much so that I am the team coordinator for the University of Newcastles Esports club Valorant team.</p>
                 <p>Feel free to contact me! </p>
+                <div className='divOfButtons'>
+                  <div className='buttonOne'>
+                    <a href='#contact' className='button'>Contact</a>
+                  </div>
+                  <div className='buttonTwo'>
+                    <button className='button'>Resume</button>
+                  </div>
+                </div>
+                <div className='leafToCenter'>
+                  <img className="leafImage" src={Leaf} alt='Drawing of a leaf'/>
+                </div>
+                <div className='iconsToCenter'>
+                  <IconLinks className='iconLinks'/>
+                </div>
+                
               </div>
               <div className='experienceSection'>
+                <h3 className='sectionHeading'>Experience</h3>
                 {experience.map((experience) => (
                 <div className='experienceItem'>
                   <h5 className='experienceDate'>{experience.date}</h5>
@@ -35,7 +53,7 @@ function About() {
                   <h4 className='experienceSubtitle'>{experience.subtitle}</h4>
                   <p className='experienceDescription'>{experience.description}</p>
                   <p className='experienceSkills'>Key skills: {experience.skillsFromExperience}</p>        
-                
+                  <hr/>
                 </div>
                 ))}
               </div>
