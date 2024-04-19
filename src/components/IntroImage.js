2020b2070f713b296494d3ad4284b615d46b9563
Emma Walker <emma.walker11@uon.edu.au>
2024-04-19 12:27:35 +1000

    cleaning up and commenting on the intro image js and css

--- src/components/IntroImage.js
+++ src/components/IntroImage.js
@@ -1,3 +1,9 @@
+//    FileName: IntroImage.js
+//    Author: Emma Walker
+//    Date last modified: 19/04/2024
+//    Description: A file for getting an image that will be modified with css to have text centered.
+//      This is at the top of the webpage with my name and a small description of myself
+
 import React from 'react'
 import Trees from '../images/SoundwaveTreesMiddle.jpg';
 import './IntroImage.css';
