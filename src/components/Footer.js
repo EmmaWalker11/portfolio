ffa7f221182a54dfecd2d86150ca7f6332d2a978
Emma Walker <emma.walker11@uon.edu.au>
2024-04-20 12:35:18 +1000

    Adding comments to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@
 //    Date last modified: 19/04/2024
 //    Description: A file for making the footer of the site
 
+// imports
 import React from 'react'
 import IconLinks from './IconLinks'
 import './Footer.css';
@@ -10,12 +11,15 @@ import './Footer.css';
 function Footer() {
   return (
     <>
+    {/* Container */}
         <div className="footerContainer">
+          {/* contact details */}
             <div className='contactDetails'>
                 <h2>Emma Walker</h2>
                 <div>Emma.Walker2410@gmail.com</div>
                 <div>0498666015</div>
             </div>
+            {/* links for external sites that have to do with me */}
             <div className='iconLinks'>
                 <IconLinks />
             </div>
